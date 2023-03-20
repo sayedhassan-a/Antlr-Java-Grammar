744294813d5a204193bdf058eeab285ba40f4e9b
Mahmoud Mousa <66203316+mahmoudmosaad82@users.noreply.github.com>
2023-03-20 21:13:39 +0200

    Update main files

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
+
+import java.io.FileWriter;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception{
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter file path");
+        String file_name = myObj.nextLine();  // Read user input
 
-        ANTLRInputStream input = new ANTLRFileStream("C:\\Users\\Sayed\\IdeaProjects\\trial\\src\\trial.java");
+        ANTLRInputStream input = new ANTLRFileStream(file_name);
         JavaLexer lexer=new JavaLexer(input);
         CommonTokenStream tokens= new CommonTokenStream(lexer);
         JavaParser parser = new JavaParser(tokens);
@@ -11,7 +17,10 @@ public class Main {
         TokenStreamRewriter rewriter = new TokenStreamRewriter(tokens);
         ParseTreeWalker walker = new ParseTreeWalker();
         walker.walk(new myBaseListener(rewriter), tree);
-        System.out.println(rewriter.getText());
+
+        FileWriter myWriter = new FileWriter("intermediate.java");
+        myWriter.write(rewriter.getText());
+        myWriter.close();
 
 
     }
