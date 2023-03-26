b44e4c09e17cef80a0419a83267041b7ddaeee50
Abdelrahman Hesham <61296209+Red-Cloud2000@users.noreply.github.com>
2023-03-27 00:41:52 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        int x=5;
+        //ANTLRFileStream input=new ANTLRFileStream("C:\\Users\\Sayed\\IdeaProjects\\trial\\src\\trial.java");
+        if(5>=4){
+            if(1==0){
+
+            }
+        }
+    }
+}
