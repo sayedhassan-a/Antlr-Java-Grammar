311ccecc0af623737558cb775884a97f662b6adb
Ziad El-3assal <84142720+ZiadSENG@users.noreply.github.com>
2023-03-27 00:41:09 +0200

    Update myBaseListener.java

--- src/myBaseListener.java
+++ src/myBaseListener.java
@@ -1,22 +1,61 @@
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.TokenStreamRewriter;
 import org.antlr.v4.runtime.tree.TerminalNode;
+
+import java.io.FileWriter;
+import java.util.Iterator;
 import java.util.Vector;
 
 public class myBaseListener extends JavaParserBaseListener{
     int block_num;
+    int cur_block;
+    boolean add_imports;
     TokenStreamRewriter rewriter;
     public myBaseListener(TokenStreamRewriter rewriter){
         this.rewriter = rewriter;
         this.block_num = 0;
+        this.cur_block = 0;
+        this.add_imports = false;
     }
+
+    @Override
+    public void enterTypeDeclaration(JavaParser.TypeDeclarationContext ctx) {
+        if(!add_imports) {
+            rewriter.insertBefore(ctx.getStart(), "import java.io.FileWriter;\n" +
+                    "import java.util.HashSet;\n" +
+                    "import java.util.Set;\n"+"import java.util.Iterator;\n");
+            add_imports = true;
+        }
+    }
+
     @Override
     public void enterBlock(JavaParser.BlockContext ctx) {
         //System.out.println(ctx.getText());
-
         //System.out.println("{//block number "+block_num+ctx.getText().substring(1));
-        rewriter.insertAfter(ctx.getStart(),"//block number"+block_num);
-        block_num++;
+        if(block_num==0) {
+            rewriter.insertAfter(ctx.getStart(), "//block number"+block_num+"\n"+"Set<String> hash_Set = new HashSet<String>();\n"+"hash_Set.add(\"block number\""+"+\" "+block_num+" \"+"+"\"is visited\");\n");
+        }
+        else{
+            rewriter.insertAfter(ctx.getStart(),"//block number"+block_num+"\n"+"hash_Set.add(\"block number\""+"+\" "+block_num+" \"+"+"\"is visited\");\n");
+        }
+
+        block_num++; //block number to indicate numbe of blocks passed
+        cur_block++; //as flag for exit to know first block
     }
 
+    @Override
+    public void exitBlock(JavaParser.BlockContext ctx) {
+        cur_block--;
+
+        if(cur_block == 0) {
+            rewriter.insertBefore(ctx.getStop(),"FileWriter myWriter = new FileWriter(\"visited.txt\");\n" +
+                    "Iterator<String> itr = hash_Set.iterator();\n" +
+                    "while(itr.hasNext()){\n" +
+                    "myWriter.write(itr.next());\n" +
+                    "myWriter.write(\'\\n\');\n" +
+                    "}\n" +
+                    "myWriter.close();\n");
+
+        }
+    }
 }
