302dc3e9d9742be1008c126e5b005f1246e82dbb
ZiadSENG <ziad.seng@gmail.com>
2023-03-28 16:31:11 +0200

    Adding recursion Function in the test

---
+++ Main.java
@@ -0,0 +1,58 @@
+import java.io.FileWriter;
+import java.util.*;
+public class Main {
+    public static void say_hi() throws Exception {
+//block number 0
+        FileWriter fileWriter0 = new FileWriter("visited.txt", true);
+        fileWriter0.write("Block number " +0+ " is visited\n");
+        fileWriter0.close();
+
+        System.out.println("Hi");
+    }
+    public static void main(String[] args) throws Exception{
+//block number 1
+        FileWriter fileWriter1 = new FileWriter("visited.txt", true);
+        fileWriter1.write("Block number " +1+ " is visited\n");
+        fileWriter1.close();
+
+        trial_rec(5);
+        int x=5;
+        //ANTLRFileStream input=new ANTLRFileStream("C:\\Users\\Sayed\\IdeaProjects\\trial\\src\\trial.java");
+        say_hi();
+        if(5>=4){
+//block number 2
+        FileWriter fileWriter2 = new FileWriter("visited.txt", true);
+        fileWriter2.write("Block number " +2+ " is visited\n");
+        fileWriter2.close();
+
+            if(1==0){
+//block number 3
+        FileWriter fileWriter3 = new FileWriter("visited.txt", true);
+        fileWriter3.write("Block number " +3+ " is visited\n");
+        fileWriter3.close();
+
+                say_hi();
+            }
+        }
+        for(int i=0;i<5;i++){
+//block number 4
+        FileWriter fileWriter4 = new FileWriter("visited.txt", true);
+        fileWriter4.write("Block number " +4+ " is visited\n");
+        fileWriter4.close();
+
+            System.out.println("Hello");
+            say_hi();
+        }
+    }
+    public static int trial_rec(int x) throws Exception{
+//block number 5
+        FileWriter fileWriter5 = new FileWriter("visited.txt", true);
+        fileWriter5.write("Block number " +5+ " is visited\n");
+        fileWriter5.close();
+
+        if(x==0) return 0;
+        System.out.println(x);
+        x-=1;
+        return trial_rec(x);
+    }
+}
--- input/Main.java
+++ input/Main.java
@@ -1,12 +1,26 @@
-
 public class Main {
+    public static void say_hi() throws Exception {
+        System.out.println("Hi");
+    }
     public static void main(String[] args) throws Exception{
+        trial_rec(5);
         int x=5;
         //ANTLRFileStream input=new ANTLRFileStream("C:\\Users\\Sayed\\IdeaProjects\\trial\\src\\trial.java");
+        say_hi();
         if(5>=4){
             if(1==0){
-
+                say_hi();
             }
         }
+        for(int i=0;i<5;i++){
+            System.out.println("Hello");
+            say_hi();
+        }
+    }
+    public static int trial_rec(int x) throws Exception{
+        if(x==0) return 0;
+        System.out.println(x);
+        x-=1;
+        return trial_rec(x);
     }
 }
