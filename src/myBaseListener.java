a5bac88a27d63e318aa80db526815c0a0f0b7dfb
Sayed Abdelrazek <sayed.abdelrazek@bld.ai>
2023-03-28 21:34:46 +0200

    Fix throwException

--- src/myBaseListener.java
+++ src/myBaseListener.java
@@ -21,7 +21,7 @@ public class myBaseListener extends JavaParserBaseListener{
     public void enterTypeDeclaration(JavaParser.TypeDeclarationContext ctx) {
         if(!add_imports) {
             rewriter.insertBefore(ctx.getStart(), "import java.io.FileWriter;\n" +
-                    "import java.util.*;\n");
+                    "import java.util.*;\nimport java.io.*;\n");
             add_imports = true;
         }
     }
@@ -31,9 +31,12 @@ public class myBaseListener extends JavaParserBaseListener{
     public void enterBlock(JavaParser.BlockContext ctx) {
         //System.out.println(ctx.getText());
         //System.out.println("{//block number "+block_num+ctx.getText().substring(1));
-        rewriter.insertAfter(ctx.getStart(), "\n//block number "+block_num+"\n"+"        FileWriter fileWriter"+block_num+" = new FileWriter(\"visited.txt\", true);\n" +
+        rewriter.insertAfter(ctx.getStart(), "\n//block number "+block_num+"\n"+"        try {FileWriter fileWriter"+block_num+" = new FileWriter(\"visited.txt\", true);\n" +
                 "        fileWriter"+block_num+".write(\"Block number \" +"+block_num +"+ \" is visited\\n\");\n" +
-                "        fileWriter"+block_num+".close();\n");
+                "        fileWriter"+block_num+".close();\n } catch (IOException e) {\n" +
+                "        System.out.println(\"An error occurred while writing to file.\");\n" +
+                "        e.printStackTrace();\n" +
+                "    }\n");
 
 
         block_num++; //block number to indicate numbe of blocks passed
