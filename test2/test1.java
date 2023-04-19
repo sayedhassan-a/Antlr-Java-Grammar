56dd423fc2cedc2a28c3aeedeb78aa39f05ee1cd
Sayed Abdelrazek <sayed.abdelrazek@bld.ai>
2023-04-19 11:33:51 +0200

    add test1

---
+++ test2/test1.java
@@ -0,0 +1,75 @@
+public class test1 {
+    public static void say_hi() {
+        System.out.println("Hi");
+    }
+    public static void main(String[] args) {
+        boolean x=true;
+        if(x){
+
+        }
+        if(!x){
+
+        }
+        trial_rec();
+    }
+    public static void trial_rec() {
+        if(true||false)
+        {
+
+        }
+        if(false||true)
+        {
+
+        }
+        if(true&&false)
+        {
+
+        }
+        if(false&&true)
+        {
+
+        }
+        if(true){
+
+        }
+        if(false)
+        {
+
+        }
+
+
+        if(true && true && true) {}
+        if(true && true && false) {}
+        if(true && false && true) {}
+        if(true && false && false) {}
+        if(false && true && true) {}
+        if(false && true && false) {}
+        if(false && false && true) {}
+        if(false && false && false) {}
+        if(true || true || true) {}
+        if(true || true || false) {}
+        if(true || false || true) {}
+        if(true || false || false) {}
+        if(false || true || true) {}
+        if(false || true || false) {}
+        if(false || false || true) {}
+        if(false || false || false) {}
+        if(true && true || true) {}
+        if(true && true || false) {}
+        if(true && false || true) {}
+        if(true && false || false) {}
+        if(false && true || true) {}
+        if(false && true || false) {}
+        if(false && false || true) {}
+        if(false && false || false) {}
+        if(true || true && true) {}
+        if(true || true && false) {}
+        if(true || false && true) {}
+        if(true || false && false) {}
+        if(false || true && true) {}
+        if(false || true && false) {}
+        if(false || false && true) {}
+        if(false || false && false) {}
+
+    }
+}
